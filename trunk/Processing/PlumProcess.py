319ec06107aace41bebd9ca193387a5e02fbc63a
deepak.subu@gmail.com <deepak.subu@gmail.com@14654f62-31a7-c725-3b93-b8ec8fd819df>
2012-01-17 03:45:55 +0000

    Corrected the logic for the difference

--- trunk/Processing/PlumProcess.py
+++ trunk/Processing/PlumProcess.py
@@ -6,116 +6,84 @@ def truncate_time(input):
 def edge_find(file_path):
     device_log=open(file_path)
     li=device_log.read()
-    init_time=734332.639600000
-    start_time=734334.64580000
-    end_time=734341.6014000000
+    init_time =734332.6319444445
+    start_time=734334.6527777778
+    end_time=734341.6111111111
     timeseries_array_str=li.split(",")[0:-1]
-#    print timeseries_array_str
-
     #add the init time
-    timeseries_array_full=[(truncate_time(float(ele))+init_time) for ele in timeseries_array_str][1:]
-    
+    timeseries_array_full=[(truncate_time(float(ele))+init_time) for ele in timeseries_array_str][0:]
     timeseries_array=[]
-
 #   truncate time series array between start and end time
     for ele in range(0,len(timeseries_array_full)):
         if ( timeseries_array_full[ele] >= start_time and timeseries_array_full[ele] <= end_time):
             timeseries_array.append(timeseries_array_full[ele])
-    
     final_timestamp=timeseries_array[len(timeseries_array)-1]
-    length=len(timeseries_array)
 
     temp_array=timeseries_array
     timeseries_array=[] 
-
     #delete the last hour of data
     for ele in temp_array: 
         if (final_timestamp-ele >= 1/24.0):        
             timeseries_array.append(ele)
-        #else:
-           # print ele
-                      
-    #print timeseries_array
+
     edge_detection_constant=truncate_time(15000.00000)#0.000173611111111
     max_edge_length=truncate_time(120000.00000)
-    edge_array=[]
-    end_array=[]
-    count_array=[]
+    edges=[]
+    ends=[]
+    counts=[]
     tcounter=1
-    #for tcounter in range(len(timeseries_array)):
     while tcounter<len(timeseries_array):
+                ##Everything is same till here  more or less there is some issue at the end of the decimal point          
         t0=timeseries_array[tcounter] 
-        edge_array.append(t0)
+        edges.append(t0)
         count=1   
-        if (tcounter==len(timeseries_array)-1):
-                break  
-        else:
-                dist_btw_motion=timeseries_array[tcounter+1]-timeseries_array[tcounter]  
-            
-        dist_btw_motion= timeseries_array[tcounter+1]-timeseries_array[tcounter]
-        while dist_btw_motion < edge_detection_constant and tcounter < len(timeseries_array):
-            tcounter=tcounter+1
-            count=count+1
-            if (tcounter==len(timeseries_array)-1):
-                break  
-            else:
-                dist_btw_motion=timeseries_array[tcounter+1]-timeseries_array[tcounter]  
+        try:
+            while timeseries_array[tcounter+1]-timeseries_array[tcounter] < edge_detection_constant and tcounter < (len(timeseries_array)-1):
+                tcounter=tcounter+1
+                count=count+1
+        except IndexError:
+            print "IndexOutOfRange"
+
             
         #declare new end 
-        end_array.append(timeseries_array[tcounter])
+        ends.append(timeseries_array[tcounter])
         #append the count of the edges
-        count_array.append(count)
+        counts.append(count)
          
         if ((timeseries_array[tcounter]-t0)>max_edge_length):
-            end_array.append(count)
-            edge_array.append(count)
-            count_array.append(-1)
+            ends.append(count)
+            edges.append(count)
+            counts.append(-1)
 
-        if (count_array[len(count_array)-1]<2):
-                count_array.pop()
-                edge_array.pop()
-                end_array.pop()
+        if (counts[len(counts)-1]<2):
+                counts.pop()
+                edges.pop()
+                ends.pop()
         tcounter=tcounter+1
-       
-       # if tcounter==(len(timeseries_array)-2):
-       #     break;
-        
-    events_from_edges(edge_array,end_array)
-#    print "end_array" , end_array
-#    print "edge_array" , edge_array
-#    print "count_array", count_array, len(count_array)
+    print edges,len(edges)
+    events_from_edges(edges,ends)
 
 def events_from_edges(edge_array,end_array):
-#    print edge_array,len(edge_array)
-
-    edge_subsumption_threshold=0.006944444
-    min_activity_length=0.00017361
+    edge_subsumption_threshold=0.006944444444
+    min_activity_length       =0.000173611111
     rejected=0
     entries=[]
     exits=[]
     ed_ctr=0
     count=0
       
-    while ed_ctr < (len(edge_array)-1):
+    while ed_ctr < len(edge_array):
         temp_edge=edge_array[ed_ctr]
-#        print temp_edge
+        try:
+            while edge_array[ed_ctr+1]-edge_array[ed_ctr]< edge_subsumption_threshold and ed_ctr < len(edge_array)-1:
+                ed_ctr=ed_ctr+1
+                count=count+1
+        except IndexError:
+            print "IndexHappilyOutOfRangeNeedToPutSomeNiceLogicHere"
     
-# % subsume following edge based on time threshold
-#            while m < length(edges{i,j}) && ...
-#                    edges{i,j}(m+1)-edges{i,j}(m) < t1
-#                count = count + 1;
-#                m = m+1;
-#            end
-        dist_btw_events=edge_array[ed_ctr+1]-edge_array[ed_ctr]
-        while dist_btw_events < edge_subsumption_threshold and ed_ctr < len(edge_array)-1:
-            ed_ctr=ed_ctr+1
-            count=count+1
-            if ed_ctr == len(edge_array)-1:
-                break
-            else:
-                dist_btw_events=edge_array[ed_ctr+1]-edge_array[ed_ctr]
-
         temp_end=end_array[ed_ctr]
+        
+        
 #        print temp_end - temp_edge, ed_ctr
         if count == 0 and temp_end-temp_edge < min_activity_length:
              #   %disp(datestr(tempEdge))         % display rejected time
@@ -128,8 +96,9 @@ def events_from_edges(edge_array,end_array):
                 exits.append(temp_end)
                 ed_ctr = ed_ctr+1
                 count = 0
-        if ed_ctr==len(edge_array):
-           break
+
+    print entries, len(entries)
+    print rejected
     actDur=findActDur(entries,exits)
     edgeData=findRelevantEdges(edge_array,end_array,entries , exits)
     t1=0.0041667
@@ -142,100 +111,54 @@ def findActDur(entries,exits):
     activity_dur = []
     for ele in range(0,len(entries)):
         activity_dur.append(exits[ele]-entries[ele])
+#    print activity_dur, len(activity_dur)
     return activity_dur
 
 def findRelevantEdges( edges , ends , entries , exits):
     edgeData =[None]*len(entries)
-
-#The most fucked up logic yet
-    for i in range(0,1):
-#        edgeData.append([None]*2)#.append = cell(length(entries{i}),1);
-        for j in range(0,len(entries)):
-            edgeData[j]=[None]*2
+    for j in range(0,len(entries)):
+        edgeData[j]=[None]*2
             #edgeData[i].append([])
-            edge_list=where(logical_and(array(edges) >= entries[j],array(edges) <=exits[j])==True,1,0)
-            templist=list(edge_list)
-            trunclist=[]
-            for ele in range(0,len(templist)):
-                if templist[ele] != 0.0:
-                    trunclist.append(edges[ele])
-            edgeData[j][0]=trunclist
-
-#           edgeData[i][0]=templist
-#edgeData[i].append([])
-            end_list=where(logical_and(array(ends) >= entries[j],array(ends) <=exits[j])==True,1,0)
-            templist=list(end_list)
-            trunclist=[]
-            for ele in range(0,len(templist)):
-                if templist[ele] != 0.0:
-                    trunclist.append(ends[ele])
-            edgeData[j][1]=trunclist
+        edge_list=where(logical_and(array(edges) >= entries[j],array(edges) <=exits[j])==True,1,0)
+        templist=list(edge_list)
+        trunclist=[]
+        for ele in range(0,len(templist)):
+            if templist[ele] != 0.0:
+                trunclist.append(edges[ele])
+        edgeData[j][0]=trunclist
+        end_list=where(logical_and(array(ends) >= entries[j],array(ends) <=exits[j])==True,1,0)
+        templist=list(end_list)
+        trunclist=[]
+        for ele in range(0,len(templist)):
+            if templist[ele] != 0.0:
+                trunclist.append(ends[ele])
+        edgeData[j][1]=trunclist
     return edgeData
 
-                    
-
-#            edgeData[i].append([])
-#            edgeData[i][1]=templist
-            #print logical_list
-#        edgeData[i][j][0] = edges[i];
-#        edgeData[i][j][1] = ends{i}(ends{i} >= entries{i}(j) & ...
-#            ends{i} <= exits{i}(j));
-#    print edgeData
-
 def splitActivity( entries , exits , edgeData , actDur , t1 , t2 ):
-    newEntry = []#[None]*len(entries)#cell(size(entries));
-    newExit =  []#[None]*len(entries)#cell(size(entries));
+    newEntry = []
+    newExit =  []
 
-    loneEnd = []#[None]*len(entries)#cell(size(entries));
+    loneEnd = []
 
-    for i in range(0,1):#for i = 1:length(entries)              % loop through each HH
-        for j in range(0,len(entries)):#for j = 1:length(entries{i})       % loop through each activity start
-            newEntry.append(entries[j])#newEntry{i} = [newEntry{i}; entries{i}(j)];   % add new entry
-            loneEnd.append(-1)#loneEnd{i} = [loneEnd{i}; -1];
-        
-#        % check activity threshold
+    for i in range(0,1):
+        for j in range(0,len(entries)):
+            newEntry.append(entries[j])
+            loneEnd.append(-1)
             if actDur[j] > t1:
                 loneEnd[-1] = 0;
-                tempEdges=edgeData[j][0]#tempEdges = edgeData{i}{j}{1};   % grab relevant edges / ends
-                tempEnds=edgeData[j][1]#tempEnds = edgeData{i}{j}{2};
-            
-#            % loop through edges
-                m = 1#            m = 2;
-                while m<(len(tempEdges)-1):#while m < length(tempEdges)
-                    if tempEdges[m+1] - tempEnds[m]>t2:#if tempEdges(m+1) - tempEnds(m) > t2
-                   #                % if edge-to-end > t2
-                        newExit.append(tempEnds[m])#newExit{i} = [newExit{i}; tempEnds(m)];
-                        newEntry.append(tempEdges[m+1])#newEntry{i} = [newEntry{i}; tempEdges(m+1)];
-                        loneEnd.append(0)#loneEnd{i} = [loneEnd{i}; 0];
-                        print "Before m",m
-                        if m+1 ==len(tempEdges):#if m+1 == length(tempEdges)
-                            loneEnd.append(1)#loneEnd{i}(end) = 1;
-                    #end
-                        print "After m",m    
+                tempEdges=edgeData[j][0]
+                tempEnds=edgeData[j][1]
+                m = 1
+                while m<(len(tempEdges)-1):
+                    if tempEdges[m+1] - tempEnds[m]>t2:
+                        newExit.append(tempEnds[m])
+                        newEntry.append(tempEdges[m+1])
+                        loneEnd.append(0)
+                        if m+1 ==len(tempEdges):
+                            loneEnd.append(1)
                         m = m+1;
-
-                #end
                     m = m+1;
-            #end
-            
-        #end
-            newExit.append(exits[j])# newExit{i} = [newExit{i}; exits{i}(j)];
-        var= raw_input(">>Enter what variable u want to inspect")
-        print ">>",newEntry[int(var)],len(newEntry)
-        
-#    end
-    
-#end
-
-#end
-
-#    end
-                       
-    
-#end
-
-#end
-
-
+            newExit.append(exits[j])
 if __name__=="__main__":
-   edge_find("/home/deepak/Development/Processing/PLUM/DemoData1/PLUM-Bhubaneswar/Week_1/Device_14/data0000.csv")
+    edge_find("/home/deepak/development/processing/PLUM/DemoData1/PLUM-Bhubaneswar/Week_1/Device_14/data0000.csv")
